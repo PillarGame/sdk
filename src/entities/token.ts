3f083421e6f646e3675e8ffe109377a9ced4b712
Ivan Borisov <2622610@gmail.com>
2021-10-23 16:35:56 +0300

    fixed package json

--- src/entities/token.ts
+++ src/entities/token.ts
@@ -1,7 +1,7 @@
 import invariant from 'tiny-invariant'
-import {ChainId} from '../constants'
-import {validateAndParseAddress} from '../utils'
-import {Currency} from './currency'
+import { ChainId } from '../constants'
+import { validateAndParseAddress } from '../utils'
+import { Currency } from './currency'
 
 /**
  * Represents an ERC20 token with a unique address and some metadata.
